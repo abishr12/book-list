2eeebd3898b321f88f3210a700527968267770a0
abishr <adhambishr@gmail.com>
2018-02-11 11:12:14 -0800

    redux working

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "Harry Potter" },
-    { title: "Eloquent Jscript" },
-    { title: "The Dark Tower" },
-    { title: "Beowulf" }
+    { title: "Harry Potter", pages: 101 },
+    { title: "Eloquent Jscript", pages: 38 },
+    { title: "The Dark Tower", pages: 566 },
+    { title: "Beowulf", pages: 79 }
   ];
 }
